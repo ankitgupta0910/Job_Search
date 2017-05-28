cf6337573cab492c112db86663d026d8155b5242
Ankit Gupta <ankitgupta09101987@gmail.com>
2017-05-27 23:10:51 -0700

    EnableMongoRepository

--- src/main/java/core/JobsearchApplication.java
+++ src/main/java/core/JobsearchApplication.java
@@ -6,12 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan({"core", "Controllers","Model", "Repository", "Utility", "Backend"})
+@ComponentScan({"core", "Controllers","Model", "Utility", "Backend"})
 //@SpringBootApplication
-@EnableJpaRepositories
+@EnableMongoRepositories("Repository")
 public class JobsearchApplication {
 
 	public static void main(String[] args) {
